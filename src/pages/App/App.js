300d7b06ba486fb5abf45687c922abc2d5948ac0
rudyards <rudy@mightycheese.com>
2019-07-10 16:20:46 -0700

    continuing set up
    goal is to have everything stubbed up in a very basic fashion by the end of today

---
+++ src/pages/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Trivia Hub</header>
+      <Switch>
+
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
