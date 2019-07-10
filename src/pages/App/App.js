d84a31c7dcb3e55b8262f3b5415ecb1545f594e5
rudyards <rudy@mightycheese.com>
2019-07-10 16:37:51 -0700

    Stubbing it up

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import GamePage from '../Game/GamePage';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Trivia Hub</header>
       <Switch>
+      <Route exact path='/' render={() => 
+        <section>
+        Welcome To Trivia Hub!
 
+        <br/>Difficulty:
+
+        <br/>Category:
+        </section>
+        }/>
+        <Route path='/Game/' render={(props) => 
+            <GamePage />
+        }/>
       </Switch>
     </div>
   );
---
+++ src/pages/Game/GamePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Game.css';
+
+const GamePage = (props) => {
+
+    return (
+        <div className="GamePage">
+            Here's the GamePage
+        </div>
+    );
+}
+
+export default GamePage;
