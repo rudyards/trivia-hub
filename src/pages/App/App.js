4ab235785bb8fd8796d671685ce0dd3f3b392ac3
rudyards <rudy@mightycheese.com>
2019-07-10 21:22:53 -0700

    establish selects

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,28 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { getAllCategories } from '../../services/trivia-api';
 import GamePage from '../Game/GamePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Trivia Hub</header>
-      <Switch>
-      <Route exact path='/' render={() => 
-        <section>
-        Welcome To Trivia Hub!
+class App extends Component  {
 
-        <br/>Difficulty:
+    state = {
+        categories: []
+    }
 
-        <br/>Category:
-        </section>
-        }/>
-        <Route path='/Game/' render={(props) => 
-            <GamePage />
-        }/>
-      </Switch>
-    </div>
-  );
+    getCategory = (idx) => {
+        return this.state.categories[idx]
+    }
+
+    async componentDidMount(){
+        const categories = await getAllCategories();
+        this.setState({ categories: categories.trivia_categories})
+    }
+
+    render() {
+        return (
+            <div className="App">
+              <header className="App-header">Trivia Hub</header>
+              <Switch>
+              <Route exact path='/' render={() => 
+                <section>
+                Welcome To Trivia Hub!
+
+                <br/>Difficulty:
+                <select name="difficulty">
+                <option value="Easy">Easy</option>
+                <option value="Medium">Medium</option>
+                <option value="Hard">Hard</option>
+                </select>
+
+                <br/>Category:
+                <select name="category">
+                {this.state.categories.map((category) =>
+                    <option value={category.id}>{category.name}</option>
+                    )}
+
+                </select>
+
+
+                <option value=""></option>
+                </section>
+                }/>
+                <Route path='/Game/' render={(props) => 
+                    <GamePage />
+                }/>
+              </Switch>
+            </div>
+          );
+    }
+
 }
 
 export default App;
--- src/services/trivia-api.js
+++ src/services/trivia-api.js
@@ -1,1 +1,5 @@
 const BASE_URL = 'https://opentdb.com/api.php?';
+
+export function getAllCategories(){
+    return fetch('https://opentdb.com/api_category.php', {mode: "cors"}).then(res => res.json());
+}
