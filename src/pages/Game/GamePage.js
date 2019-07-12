52be66dda20efdb8dfeec883f830f2c2a8947e11
rudyards <rudy@mightycheese.com>
2019-07-11 17:09:25 -0700

    questions now display!

--- src/pages/Game/GamePage.js
+++ src/pages/Game/GamePage.js
@@ -4,17 +4,36 @@ import { getQuestions } from '../../services/trivia-api';
 
 class GamePage extends Component {
 
+    state = {
+        questions: []
+    }
+
     async componentDidMount(){
-        const questions = await getQuestions(this.props.difficulty, this.props.category);
-        console.log(questions)
+        const theseQuestions = await getQuestions(this.props.difficulty, this.props.category);
+        this.setState({questions: theseQuestions.results})
+        console.log(this.state.questions[0].question)
     }
 
     render(props){
-    return (
-        <div className="GamePage">
-            Here's the GamePage
-        </div>
-    );
+        const renderHTML = (escapedHTML: string) => React.createElement("div", { dangerouslySetInnerHTML: { __html: escapedHTML } });
+
+        // var eachQuestion = this.state.questions.map((q, i) => {
+        //     <li>
+        //     {q}
+        //     </li>
+        // })
+        return (
+            <div className="GamePage">
+                Here's the GamePage
+                <ul>
+                { this.state.questions.map((q, i) =>
+                    <p>
+                    {renderHTML(q.question)}
+                    </p>
+                    )}
+                </ul>
+            </div>
+        );
     }
 }
 
