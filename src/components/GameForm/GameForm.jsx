a471f8d4d66fdafb484cfdac243350b070344027
rudyards <rudy@mightycheese.com>
2020-03-24 12:07:14 -0700

    Text tweaks

--- src/components/GameForm/GameForm.jsx
+++ src/components/GameForm/GameForm.jsx
@@ -8,14 +8,14 @@ class GameForm extends Component {
         return(
         <form className="form-horizontal form-container" onSubmit={this.props.handleGameStart}>
             <br/>
-            <div class="game-form">Difficulty:
+            <div class="game-form">Difficulty: &nbsp;
             <select name="difficulty" onChange={this.props.handleGameChange} value={this.props.difficulty}>
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
                 <option value="hard">Hard</option>
             </select>
 
-            <br/>Category:
+            <br/>Category:  &nbsp;
             <select name="category" onChange={this.props.handleGameChange} value={this.props.category}>
                 {this.props.categories.map((category, idx) =>
                     <option key={idx} value={category.id}>{category.name}</option>
